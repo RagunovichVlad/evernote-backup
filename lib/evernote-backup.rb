b1b3664e68f1b05ce0390dae899faf7a3da0ce2e
Vlad Ragunovich <vlad.ragunovich@gmail.com>
2017-02-28 16:12:31 +0300

    Add configuration for Evernote and AWS

--- lib/evernote-backup.rb
+++ lib/evernote-backup.rb
@@ -1,4 +1,12 @@
-require "rollbar_api/version.rb"
+require 'active_support/core_ext/module/attribute_accessors'
+require "evernote_oauth"
+require 'aws-sdk'
+require "evernote-backup/version"
+require "evernote-backup/evernote/client"
+require "evernote-backup/exporter"
+require "evernote-backup/storage/aws"
+require "evernote-backup/configuration"
 
 module EvernoteBackup
+  extend Configuration
 end
